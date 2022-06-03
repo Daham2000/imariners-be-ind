25222ccd4111d04b269f57070478dc9c5d10af21
Daham2000 <dahamakalanka2000@gmail.com>
2022-06-03 10:21:22 +0530

    deploy procfile added

--- src/routes/category_router/category_router.ts
+++ src/routes/category_router/category_router.ts
@@ -4,9 +4,6 @@ import multer from "multer";
 
 const uploader = multer({ dest: 'uploads/' })
 
-
-
-
 const categoryRouter = Router();
 
 categoryRouter.route("/").post(CategoryHandler.addCategory);
