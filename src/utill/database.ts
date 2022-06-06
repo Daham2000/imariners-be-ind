1875636293c3d64c206690d02af437f59ed177c8
Daham2000 <dahamakalanka2000@gmail.com>
2022-06-06 11:18:53 +0530

    Database security

--- src/utill/database.ts
+++ src/utill/database.ts
@@ -1,22 +1,16 @@
-import {Connection} from "mysql";
-import mysql from 'mysql';
-let db: Connection;
-export function init(): void {
-    if(!db){
-        db = mysql.createConnection({
-            host: "database-1.cxyzvpyv6wa8.us-west-2.rds.amazonaws.com",
-            port: 3306,
-            user: "admin",
-            password: "1234$#!qwerQ",
-            database: "my_db12"
-        });
-    }
-}
-
-export function getConnection(): Connection{
-    if(!db) {
-        throw new Error('The db pool has not been initialized, call init({}) prior to get().');
-    }
-    return db;
-}
-
+// import {Connection} from "mysql";
+// import mysql from 'mysql';
+// let db: Connection;
+// export function init(): void {
+//     if(!db){
+//
+//     }
+// }
+//
+// export function getConnection(): Connection{
+//     if(!db) {
+//         throw new Error('The db pool has not been initialized, call init({}) prior to get().');
+//     }
+//     return db;
+// }
+//
